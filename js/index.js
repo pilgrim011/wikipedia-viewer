80727b0cf13423b38d86ea84d25979c434028c87
pilgrim011 <printerstampac@gmail.com>
2017-05-17 02:57:05 +0200

    added on enter event

--- js/index.js
+++ js/index.js
@@ -1,22 +1,33 @@
 $(document).ready(function(){
-  $("img").on("click", function(){
+
+  $("#icon").on("click", function(){
     $("img").remove();
     $("h3").remove();
     $("#search").html("<form><input type=text name=query id = what><input type=button value=Search id = find></form>");
-    $("#find").click(function(){
+
+    $("#search").on("keypress", "#what", function(e){
+     if(e.which === 13){
+       e.preventDefault();
+            $("#find").click();
+
+        }
+});
+    $("#search").on("click", "#find", function(){
       var input = document.getElementById("what").value;
       var wiki = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&limit=10&namespace=0&utf8&snippet&format=json&$parse&callback=?";
  $.getJSON(wiki, function(data){
  for (var i=0;i<10;i++){
-   
+
  $("#lista").append("<a href=" + data[3][i] + ">" +
 "<li>" + data[1][i] + "</li>" +
   "<p>" + data[2][i] + "</p>" +
 "</a>");
-  
+
  }
  });
+
     });
+
   });
-  
+
 });
